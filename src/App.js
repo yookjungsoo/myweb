0f40708a650f4b84017fafd07979a79b8f88ba9b
yookjungsoo <alsue2000@gmail.com>
2021-02-20 21:00:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+    <p>안녕하세요</p>
+    <Button primary> hihihi</Button>
+    <Progress percent={66} />
+
+    1212123
+    </div>
+  );
+}
+
+export default App;
