7c26bb98b4a07c8994f213b7d63f1cf24023f6a0
yookjungsoo <alsue2000@gmail.com>
2021-02-21 01:19:17 +0900

    1111

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Progress, Image} from 'semantic-ui-react'
+import ar from "./arin.jpg"
+
+import Comments from './comment.js'
 
 function App() {
   return (
     <div>
     <p>안녕하세요</p>
-    <Button primary> hihihi</Button>
-    <Progress percent={66} />
+    <Button primary> Test1 </Button>
+    <Button> hihihi</Button>
+    <Progress percent={66} indicating /> 
+    <Image src = {ar} centered size='medium'/>
+    <Comments/>
     </div>
   );
 }
---
+++ src/login.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import './App.css';
+// import 'semantic-ui-css/semantic.min.css';
+// import {Button, Progress, Input} from 'semantic-ui-react'
+// import ar from "./arin.jpg"
+
+
+// class Login extends React.Component {
+
+//     constructor(){
+//     super()
+//     this.state = {id : "choco"}
+//   }
+
+//   render(){
+//     return(
+//     <div>
+//       <Input onChange = { (e) => this.setState({id : e.target.value})}/>
+//       <Button value = {this.state.id} onClick = { (event) => {
+//         console.log(event)
+//         alert(event.target.value)
+//       }}> Login </Button>
+//     </div>
+//     )
+//   }
+// }
+
+
+
+// function App() {
+//   return (
+//     <div>
+//     <p>안녕하세요</p>
+//     <Button primary> 123123</Button>
+//     <Button> hihihi</Button>
+//     <Progress percent={66} indicating /> 
+//     <img src = {ar}/>
+//     <Login / >
+//     </div>
+//   );
+// }
+
+// export default App;
